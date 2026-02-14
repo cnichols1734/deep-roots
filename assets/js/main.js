7b4ebe3f19bcf3071795a227578df54c6bd3643d
christophernichols <chrisnichols17@gmail.com>
2026-02-13 21:13:25 -0600

    Update: premium loader, leaf logo, local hero image, remove competitive context

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
 (function () {
+  const loader = document.querySelector('.loader');
+  if (loader) {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        loader.classList.add('hidden');
+        document.body.classList.add('loaded');
+      }, 1500);
+    });
+  }
+
   const toggle = document.querySelector('[data-nav-toggle]');
   const mobileNav = document.querySelector('[data-mobile-nav]');
 
